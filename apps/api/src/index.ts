a4a43cb8bff262a22f4f62405963a440676b71d8
naruse <62323683+naruse666@users.noreply.github.com>
2024-08-02 22:28:16 +0900

    feat: add routes (#18)
    * feat: add routes
    
    * fix: add guardian, sunshine
    
    * fix: routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -6,4 +6,23 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+const v1 = new Hono()
+v1.get('/', (c) => {
+  return c.text('v1')
+})
+
+const guardian = new Hono()
+guardian.get('/', (c) => {
+  return c.text('guardian')
+})
+v1.route('guardian', guardian)
+
+const sunshine = new Hono()
+sunshine.get('/', (c) => {
+  return c.text('sunshine')
+})
+v1.route('/sunshine', sunshine)
+
+app.route('/v1', v1)
+
 export default app
