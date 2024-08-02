03574611859e7296ca3312ae79ca0ef8f38bc62f
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-03 07:26:47 +0900

    feat: add guardians types

---
+++ packages/shared/src/types/guardian.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+import {
+  categoryScoresSchema,
+  guardianTextRequestSchema,
+  guardianImageRequestSchema,
+  guardianResultSchema,
+} from '../schemas/guardian'
+
+export type Category = keyof z.infer<typeof categoryScoresSchema>
+
+export type CategoryScores = z.infer<typeof categoryScoresSchema>
+
+export type GuardianTextDTO = z.infer<typeof guardianTextRequestSchema>
+
+export type GuardianImageDTO = z.infer<typeof guardianImageRequestSchema>
+
+export type GuardianResult = z.infer<typeof guardianResultSchema>
