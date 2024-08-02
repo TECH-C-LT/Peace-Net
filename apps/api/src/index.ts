b9cf99affe0fed1fb1757cb7573fd3b304e2cdef
naruse666 <0x706f6b6f@gmail.com>
2024-08-02 21:26:18 +0900

    feat: add routes

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -6,4 +6,28 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get('/text', (c) => {
+  return c.text('text')
+})
+
+app.get('/usage', (c) => {
+  return c.text('usage')
+})
+
+const apikey = new Hono()
+const auth = new Hono()
+auth.get('/', (c) => {
+  return c.text('apikey auth')
+})
+
+apikey.route('/auth', auth)
+
+const generate = new Hono()
+generate.get('/', (c) => {
+  return c.text('apikey generate')
+})
+apikey.route('/generate', generate)
+
+app.route('/apikey', apikey)
+
 export default app
