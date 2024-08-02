c8303cc68f96fd52a715281fdbadd0424d2923ea
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-02 20:32:47 +0900

    chore: remove docs

--- apps/docs/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@peace-net/eslint-config/next.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
