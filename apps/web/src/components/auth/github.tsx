8290018969b84aec153a8cae57e85036f8a4ac01
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-05 03:20:04 +0900

    chore: update GitHub authentication form

--- apps/web/src/components/auth/github.tsx
+++ apps/web/src/components/auth/github.tsx
@@ -6,14 +6,16 @@ import { useActionState } from 'react'
 import { signInWithGithub } from '~/server/actions/auth'
 
 export default function GitHub() {
-  const [_, action, pending] = useActionState(signInWithGithub, null)
+  const [_, signInAction, isPending] = useActionState(signInWithGithub, null)
 
   return (
-    <form action={action} className="flex justify-center px-4 py-3">
-      <Button className="w-full" size="lg" disabled={pending}>
+    <form action={signInAction} className="flex justify-center px-4 py-3">
+      <Button className="w-full" size="lg" disabled={isPending}>
         <GithubIcon className="mr-2 h-5 w-5" />
         GitHubでログイン
-        {pending && <Loader2Icon className="ml-2 h-5 w-5 animate-spin" />}
+        {isPending && (
+          <Loader2Icon className="ml-2 h-5 w-5 animate-spin ease-in-out" />
+        )}
       </Button>
     </form>
   )
