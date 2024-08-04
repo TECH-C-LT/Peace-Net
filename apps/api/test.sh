32942e02abf70fa57a84bf30cbe930676f49261f
naruse666 <0x706f6b6f@gmail.com>
2024-08-04 21:24:18 +0900

    fix: disable comment out

--- apps/api/test.sh
+++ apps/api/test.sh
@@ -17,22 +17,22 @@ source ./helpers.sh
 #
 
 ## hate
-# assert-top-scores "東大医学部頭悪くないか？" hate
+assert-top-scores "東大医学部頭悪くないか？" hate
 
 ## harassment
-# assert-top-scores "プロジェクトから外すぞ?" harassment
+assert-top-scores "プロジェクトから外すぞ?" harassment
 
 ## self_halm
-# assert-top-scores "俺生きてる意味ないよな。" self_harm
+assert-top-scores "俺生きてる意味ないよな。" self_harm
 
 ## violence
-# assert-top-scores "戦争大賛成！！" violence
+assert-top-scores "戦争大賛成！！" violence
 
 ## discrimination
 #
 
 ## No problem case
-# assert-flagged "君のパスはよかったけど、シュートは全然ダメだったね。" false
-# assert-flagged "ゲームばかりしてるとろくな大人にならないよ。" false
-# assert-flagged "それってあなたの感想ですよね？" false
-# assert-flagged "君の意見は間違っている！" false
+assert-flagged "君のパスはよかったけど、シュートは全然ダメだったね。" false
+assert-flagged "ゲームばかりしてるとろくな大人にならないよ。" false
+assert-flagged "それってあなたの感想ですよね？" false
+assert-flagged "君の意見は間違っている！" false
