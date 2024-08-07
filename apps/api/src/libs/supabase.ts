307d1440cb3fd5476a7a39be4d31e6f4fcbbba00
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-07 17:21:38 +0900

    chore: change service role client

--- apps/api/src/libs/supabase.ts
+++ apps/api/src/libs/supabase.ts
@@ -2,6 +2,6 @@ import { Database } from '@peace-net/shared/types/database'
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-export const SupabaseClient = (url: string, anonKey: string) => {
-  return createClient<Database>(url, anonKey)
+export const SupabaseClient = (url: string, serviceRoleKey: string) => {
+  return createClient<Database>(url, serviceRoleKey)
 }
