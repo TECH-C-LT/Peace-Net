49ec7d20f24f78147687f4f3b979754f1c82bc94
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-08 20:48:50 +0900

    feat: add generate apikey form

---
+++ apps/web/src/app/(main)/dashboard/api-keys/components/generate-api-key/confirm.tsx
@@ -0,0 +1,24 @@
+import { CopyButton } from '@peace-net/ui/components/ui/copy-button'
+import { Input } from '@peace-net/ui/components/ui/input'
+import { CheckCircle2Icon, CheckIcon } from 'lucide-react'
+import React from 'react'
+
+export default function GenerateApiKeyConfirm({ apiKey }: { apiKey: string }) {
+  return (
+    <div className="space-y-3">
+      <h3 className="text-lg">
+        <CheckCircle2Icon className="mb-0.5 mr-2 inline-block h-6 w-6 text-teal-500" />
+        APIキーの生成が完了しました
+      </h3>
+      <p className="text-xs">
+        このシークレットキーを安全でアクセスしやすい場所に保存してください。
+        <br />
+        セキュリティ上の理由から、再度閲覧することはできません。このAPIキーを紛失した場合は、新しいものを生成する必要があります。
+      </p>
+      <div className="my-3 flex gap-1">
+        <Input value={apiKey} readOnly className="border-teal-500" />
+        <CopyButton value={apiKey} variant="default" />
+      </div>
+    </div>
+  )
+}
