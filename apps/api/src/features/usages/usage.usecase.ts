7943535bebeee6869a2b64555188c91f2cbff1f6
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-09 03:35:13 +0900

    feat: usage features

---
+++ apps/api/src/features/usages/usage.usecase.ts
@@ -0,0 +1,46 @@
+import {
+  UnauthorizedError,
+  UsageLimitExceededError,
+} from '@peace-net/shared/core/error'
+import { failure, Result, success } from '@peace-net/shared/utils/result'
+
+import { CheckUsageInput, CheckUsageOutput } from '~/features/usages/usage.type'
+import { IUserPlanService } from '~/features/userPlans/userPlan.service'
+
+export interface IUsageUsecase {
+  checkUsage(input: CheckUsageInput): Promise<Result<CheckUsageOutput>>
+}
+
+export class UsageUsecase implements IUsageUsecase {
+  constructor(private userPlanService: IUserPlanService) {}
+
+  async checkUsage(input: CheckUsageInput): Promise<Result<CheckUsageOutput>> {
+    const { userId } = input
+
+    try {
+      // ユーザーのプラン情報を取得
+      const userPlan = await this.userPlanService.getUserPlan(userId)
+
+      if (!userPlan) {
+        return failure(new UnauthorizedError('User plan not found'))
+      }
+
+      // リクエスト数が制限を超えている場合はエラーを返す
+      if (
+        userPlan.total_requests_used !== null &&
+        userPlan.total_requests_used >=
+          (userPlan.plans?.total_request_limit ?? 0)
+      ) {
+        return failure(
+          new UsageLimitExceededError("User plan's usage limit exceeded"),
+        )
+      }
+
+      return success({
+        isValid: true,
+      })
+    } catch (error: any) {
+      return failure(error)
+    }
+  }
+}
