b51c1a4b1c67e69494e41e5abdb6ec90b0e29b5a
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-08 20:55:56 +0900

    fix: import path

--- packages/ui/src/components/ui/calendar.tsx
+++ packages/ui/src/components/ui/calendar.tsx
@@ -5,7 +5,7 @@ import { ChevronLeft, ChevronRight } from 'lucide-react'
 import { DayPicker } from 'react-day-picker'
 
 import { cn } from '@peace-net/shared/utils/classes'
-import { buttonVariants } from 'src/components/ui/button'
+import { buttonVariants } from './button'
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
