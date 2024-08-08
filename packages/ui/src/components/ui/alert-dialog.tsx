f78a1a23f51635733cd2b54c41807e451ca22e7f
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-08 22:52:01 +0900

    refactor: style

--- packages/ui/src/components/ui/alert-dialog.tsx
+++ packages/ui/src/components/ui/alert-dialog.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog'
 
 import { cn } from '@peace-net/shared/utils/classes'
-import { buttonVariants } from 'src/components/ui/button'
+import { buttonVariants } from './button'
 
 const AlertDialog = AlertDialogPrimitive.Root
 
