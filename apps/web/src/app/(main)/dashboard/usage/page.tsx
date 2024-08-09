d29cf1c367db4a75a667896d464cd4009a5e8504
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-09 14:21:04 +0900

    feat: add usage page

--- apps/web/src/app/(main)/dashboard/usage/page.tsx
+++ apps/web/src/app/(main)/dashboard/usage/page.tsx
@@ -3,6 +3,7 @@ import { Suspense } from 'react'
 import { MainTitle } from '~/app/(main)/components/text'
 import UsageLogChart from './components/usage-log-chart'
 import PlanRequestChart from './components/plan-request-chart'
+import { Skeleton } from '@peace-net/ui/components/ui/skeleton'
 
 export const metadata: Metadata = {
   title: 'Usage',
@@ -12,12 +13,23 @@ export default async function Usage() {
   return (
     <section>
       <MainTitle>Usage</MainTitle>
-      <Suspense fallback={<div>Loading...</div>}>
-        <UsageLogChart />
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <PlanRequestChart />
-      </Suspense>
+      <div className="flex flex-row-reverse flex-wrap justify-center overflow-hidden rounded-lg border p-4">
+        <Suspense
+          fallback={
+            <div className="flex flex-col items-center justify-center gap-4 p-6">
+              <h3 className="font-semibold">合計使用回数</h3>
+              <Skeleton className="h-[144] w-[144] rounded-full" />
+            </div>
+          }
+        >
+          <PlanRequestChart />
+        </Suspense>
+        <Suspense
+          fallback={<Skeleton className="h-[300] w-[550] rounded-md" />}
+        >
+          <UsageLogChart />
+        </Suspense>
+      </div>
     </section>
   )
 }
