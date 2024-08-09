f7467c3be76ec56befd588041c85d9d095d29b40
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-09 17:24:34 +0900

    🌈 style(web): add card
    add card layout

--- apps/web/src/app/(main)/dashboard/usage/page.tsx
+++ apps/web/src/app/(main)/dashboard/usage/page.tsx
@@ -1,9 +1,15 @@
 import { Metadata } from 'next'
 import { Suspense } from 'react'
-import { MainTitle } from '~/app/(main)/components/text'
 import UsageLogChart from './components/usage-log-chart'
 import PlanRequestChart from './components/plan-request-chart'
 import { Skeleton } from '@peace-net/ui/components/ui/skeleton'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@peace-net/ui/components/ui/card'
 
 export const metadata: Metadata = {
   title: 'Usage',
@@ -12,24 +18,31 @@ export const metadata: Metadata = {
 export default async function Usage() {
   return (
     <section>
-      <MainTitle>Usage</MainTitle>
-      <div className="flex flex-row-reverse flex-wrap justify-center overflow-hidden rounded-lg border p-4">
-        <Suspense
-          fallback={
-            <div className="flex flex-col items-center justify-center gap-4 p-6">
-              <h3 className="font-semibold">合計使用回数</h3>
-              <Skeleton className="h-[144px] w-[144px] rounded-full" />
-            </div>
-          }
-        >
-          <PlanRequestChart />
-        </Suspense>
-        <Suspense
-          fallback={<Skeleton className="h-[300px] w-[550px] rounded-md" />}
-        >
-          <UsageLogChart />
-        </Suspense>
-      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Usage</CardTitle>
+          <CardDescription>使用状況を確認できます。</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-row-reverse flex-wrap justify-center overflow-hidden rounded-lg border p-4">
+            <Suspense
+              fallback={
+                <div className="flex flex-col items-center justify-center gap-4 p-6">
+                  <h3 className="font-semibold">合計使用回数</h3>
+                  <Skeleton className="h-[144px] w-[144px] rounded-full" />
+                </div>
+              }
+            >
+              <PlanRequestChart />
+            </Suspense>
+            <Suspense
+              fallback={<Skeleton className="h-[300px] w-[550px] rounded-md" />}
+            >
+              <UsageLogChart />
+            </Suspense>
+          </div>
+        </CardContent>
+      </Card>
     </section>
   )
 }
