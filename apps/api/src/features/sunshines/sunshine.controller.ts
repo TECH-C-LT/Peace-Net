c8ec2d828b844c488be280c6ffc1d63e7f4d712f
waya279 <sch.21060007wh@gmail.com>
2024-08-10 18:04:37 +0900

    feat: add sunshine.usecase for route and controller

--- apps/api/src/features/sunshines/sunshine.controller.ts
+++ apps/api/src/features/sunshines/sunshine.controller.ts
@@ -2,16 +2,30 @@ import { SunshineTextDTO } from '@peace-net/shared/types/sunshine'
 import { handleError } from '@peace-net/shared/utils/error-handler'
 import { Context } from 'hono'
 
+import { ISunshineUseCase } from './sunshine.usecase'
+
 export class SunshineController {
-  constructor() {}
+  constructor(private sunshineUseCase: ISunshineUseCase) {}
 
   async sunshineText(c: Context) {
     try {
       const dto = (await c.req.json()) as SunshineTextDTO
 
-      // TODO: useCaseを追加する。
+      // TODO: get userId and apiKeyId from the request
+      const userId = 'userId'
+      const apiKeyId = 'apiKeyId'
 
-      return c.json(dto)
+      const result = await this.sunshineUseCase.sunshineText({
+        ...dto,
+        userId,
+        apiKeyId,
+      })
+
+      if (!result.ok) {
+        throw result.error
+      }
+
+      return c.json(result.value)
     } catch (error) {
       return handleError(c, error)
     }
