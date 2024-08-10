834096700447c87c880b8591f41224a878bd7ae0
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-10 22:14:14 +0900

    🦄 refactor(api): use usage facade
    - Implement UsageFacade in Guardian and Sunshine use cases

--- apps/api/src/features/sunshines/sunshine.usecase.ts
+++ apps/api/src/features/sunshines/sunshine.usecase.ts
@@ -5,8 +5,7 @@ import {
 import { failure, success, type Result } from '@peace-net/shared/utils/result'
 
 import { ISunshineService } from '~/features/sunshines/sunshine.service'
-import { IUsageLogService } from '~/features/usageLogs/usageLog.service'
-import { IUserPlanService } from '~/features/userPlans/userPlan.service'
+import { IUsageFacade } from '~/features/usages/usage.facade'
 
 export interface ISunshineUseCase {
   sunshineText(input: SunshineTextInput): Promise<Result<SunshineResult>>
@@ -15,8 +14,7 @@ export interface ISunshineUseCase {
 export class SunshineUseCase implements ISunshineUseCase {
   constructor(
     private SunshineService: ISunshineService,
-    private userPlanService: IUserPlanService,
-    private usageLogService: IUsageLogService,
+    private usageFacade: IUsageFacade,
   ) {}
 
   async sunshineText(
@@ -26,12 +24,7 @@ export class SunshineUseCase implements ISunshineUseCase {
       const { text, userId, apiKeyId } = input
       const result = await this.SunshineService.sunshineText(text)
 
-      // increment user plans total requests used
-      await this.userPlanService.incrementTotalRequestsUsed(userId)
-
-      // increment usage logs
-      // update api keys last used
-      await this.usageLogService.incrementUsageLog(apiKeyId, 'sunshines')
+      await this.usageFacade.incrementUsage(userId, apiKeyId, 'sunshines')
 
       return success({
         text: result.text,
