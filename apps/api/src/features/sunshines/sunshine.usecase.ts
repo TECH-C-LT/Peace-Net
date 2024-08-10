d120d8f2a7c83a25c143888b46b567237cf42c11
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-10 21:23:19 +0900

    ✨ feat(api): sunshine usage count
    - get userId and apiKeyId from the request
    - increment user plans total requests used
    - increment usage logs
    - update api keys last used

--- apps/api/src/features/sunshines/sunshine.usecase.ts
+++ apps/api/src/features/sunshines/sunshine.usecase.ts
@@ -5,27 +5,33 @@ import {
 import { failure, success, type Result } from '@peace-net/shared/utils/result'
 
 import { ISunshineService } from '~/features/sunshines/sunshine.service'
+import { IUsageLogService } from '~/features/usageLogs/usageLog.service'
+import { IUserPlanService } from '~/features/userPlans/userPlan.service'
 
 export interface ISunshineUseCase {
   sunshineText(input: SunshineTextInput): Promise<Result<SunshineResult>>
 }
 
 export class SunshineUseCase implements ISunshineUseCase {
-  constructor(private SunshineService: ISunshineService) {}
+  constructor(
+    private SunshineService: ISunshineService,
+    private userPlanService: IUserPlanService,
+    private usageLogService: IUsageLogService,
+  ) {}
 
   async sunshineText(
     input: SunshineTextInput,
   ): Promise<Result<SunshineResult>> {
     try {
-      const { text } = input
+      const { text, userId, apiKeyId } = input
       const result = await this.SunshineService.sunshineText(text)
 
-      // TODO: increment user plans total requests used
-      // await this.userPlanService.incrementTotalRequestsUsed(userId)
+      // increment user plans total requests used
+      await this.userPlanService.incrementTotalRequestsUsed(userId)
 
-      // TODO: increment usage logs
-      // TODO: update api keys last used
-      // await this.usageLogService.incrementUsageLog(apiKeyId, 'guardians')
+      // increment usage logs
+      // update api keys last used
+      await this.usageLogService.incrementUsageLog(apiKeyId, 'sunshines')
 
       return success({
         text: result.text,
