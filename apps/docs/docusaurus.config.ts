1747bcaf5d2056765bce020c8706ee971f12d13d
naruse <62323683+naruse666@users.noreply.github.com>
2024-08-11 12:51:21 +0900

    docs: sunshine (#137)

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: 'latest',
+              label: '1.2.1 (latest)',
               path: '/',
             },
           },
