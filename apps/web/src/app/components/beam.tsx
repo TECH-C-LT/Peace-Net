85e1218cb8a199d15ced7eb827b5e5a9a67b8280
RYU <takahashi.ryunosuke.job@gmail.com>
2024-08-13 18:56:35 +0900

    ✨ feat(web): change homepage

---
+++ apps/web/src/app/components/beam.tsx
@@ -0,0 +1,97 @@
+'use client'
+
+import { cn } from '@peace-net/shared/utils/classes'
+import { AnimatedBeam } from '@peace-net/ui/components/magicui/animated-beam'
+import {
+  AppWindowMacIcon,
+  MonitorSmartphone,
+  MonitorSmartphoneIcon,
+  TabletSmartphoneIcon,
+} from 'lucide-react'
+import Image from 'next/image'
+import React, { forwardRef, useRef } from 'react'
+
+const Circle = forwardRef<
+  HTMLDivElement,
+  { className?: string; children?: React.ReactNode }
+>(({ className, children }, ref) => {
+  return (
+    <div
+      ref={ref}
+      className={cn(
+        'border-border z-10 flex size-12 items-center justify-center rounded-full border-2 bg-white p-3 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  )
+})
+
+Circle.displayName = 'Circle'
+
+export function AnimatedBeamMultipleOutputDemo({
+  className,
+}: {
+  className?: string
+}) {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const div1Ref = useRef<HTMLDivElement>(null)
+  const div2Ref = useRef<HTMLDivElement>(null)
+  const div3Ref = useRef<HTMLDivElement>(null)
+  const div6Ref = useRef<HTMLDivElement>(null)
+
+  return (
+    <div
+      className={cn(
+        'absolute right-1/2 top-2 flex w-full max-w-md translate-x-1/2 items-center justify-center overflow-hidden rounded-lg p-10',
+        className,
+      )}
+      ref={containerRef}
+    >
+      <div className="flex size-full max-w-lg flex-row items-stretch justify-between gap-10">
+        <div className="flex flex-col justify-center gap-2">
+          <Circle ref={div1Ref}>
+            <Icons.googleDrive />
+          </Circle>
+          <Circle ref={div2Ref}>
+            <Icons.googleDocs />
+          </Circle>
+          <Circle ref={div3Ref}>
+            <Icons.whatsapp />
+          </Circle>
+        </div>
+        <div className="flex flex-col justify-center">
+          <Circle ref={div6Ref} className="size-16">
+            <Icons.openai />
+          </Circle>
+        </div>
+      </div>
+
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div1Ref}
+        toRef={div6Ref}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div2Ref}
+        toRef={div6Ref}
+      />
+      <AnimatedBeam
+        containerRef={containerRef}
+        fromRef={div3Ref}
+        toRef={div6Ref}
+      />
+    </div>
+  )
+}
+
+const Icons = {
+  openai: () => (
+    <Image src="/peace-net.webp" alt="peace-net" width={50} height={50} />
+  ),
+  googleDrive: () => <AppWindowMacIcon />,
+  whatsapp: () => <MonitorSmartphoneIcon />,
+  googleDocs: () => <TabletSmartphoneIcon />,
+}
---
+++ apps/web/src/app/components/hero.tsx
@@ -0,0 +1,39 @@
+import { cn } from '@peace-net/shared/utils/classes'
+import { Badge } from '@peace-net/ui/components/ui/badge'
+import { buttonVariants } from '@peace-net/ui/components/ui/button'
+import { GithubIcon } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+
+import { GetStartedButton } from '~/components/auth/start-button'
+import { GITHUB_URL } from '~/lib/config'
+
+export default function Hero() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-5 p-3">
+      <div className="mt-6 space-y-2 md:mt-0">
+        <Badge variant="secondary">Beta</Badge>
+        <h1 className="my-2 text-5xl font-bold">
+          平和ネット API<span className="text-teal-500">.</span>
+        </h1>
+        <h2 className="mt-[.5rem]">
+          <span className="text-teal-500">平和なインターネット</span>
+          を目指すAPIを提供します。
+        </h2>
+      </div>
+      <Image src="/peace-net.webp" alt="peace-net" width={150} height={150} />
+      <GetStartedButton />
+      <a
+        href={GITHUB_URL}
+        target="_blank"
+        className={cn(
+          buttonVariants({ variant: 'link' }),
+          'mr-5 tracking-wider',
+        )}
+      >
+        <GithubIcon className="mr-2 h-5 w-5" />
+        GitHub
+      </a>
+    </div>
+  )
+}
