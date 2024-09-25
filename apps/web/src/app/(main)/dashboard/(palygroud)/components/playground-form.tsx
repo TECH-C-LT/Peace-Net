4dc7b5f493850b564209bee8eccf3aeeccacc082
RYU <takahashi.ryunosuke.job@gmail.com>
2024-09-26 03:57:26 +0900

    feat(web): split playground forms

--- apps/web/src/app/(main)/dashboard/(palygroud)/components/playground-form.tsx
+++ apps/web/src/app/(main)/dashboard/(palygroud)/components/playground-form.tsx
@@ -1,25 +1,20 @@
 'use client'
 
 import { getFormProps, useForm } from '@conform-to/react'
-import { getZodConstraint, parseWithZod } from '@conform-to/zod'
-import { SiAnthropic, SiGoogle, SiOpenai } from '@icons-pack/react-simple-icons'
-import { Button } from '@peace-net/ui/components/ui/button'
+import { getZodConstraint } from '@conform-to/zod'
 import { Label } from '@peace-net/ui/components/ui/label'
-import { Loader2Icon, PlayIcon } from 'lucide-react'
 import { useActionState } from 'react'
 
 import { Field, FieldError } from '~/components/common/field'
 import { RadioGroupConform } from '~/components/conform/radio-group'
 import { SelectConform } from '~/components/conform/select'
-import { SliderConform } from '~/components/conform/slider'
-import { TextareaConform } from '~/components/conform/textarea'
 
 import { handlePlayground } from '../actions'
 import type { Playground } from '../schemas'
 import { playgroundSchema } from '../schemas'
-import LimitReachedDialog from './limit-reached-dialog'
-import { ResultSyntax } from './result-syntax'
-import ScoreThresholdDescription from './score-threshold-description'
+import { GuardianImageForm } from './form/guardian-image'
+import { GuardianTextForm } from './form/guardian-text'
+import { SunshineForm } from './form/sunshine'
 
 function isSuccessResult(
   result: any,
@@ -29,33 +24,24 @@ function isSuccessResult(
 }
 
 export function PlaygroundForm() {
-  const [lastResult, action, isPending] = useActionState(handlePlayground, null)
+  const [lastResult, _action, _isPending] = useActionState(
+    handlePlayground,
+    null,
+  )
   const [form, fields] = useForm({
     constraint: getZodConstraint(playgroundSchema),
     lastResult,
-    onValidate({ formData }) {
-      return parseWithZod(formData, { schema: playgroundSchema })
-    },
-    shouldRevalidate: 'onInput',
     defaultValue: {
       api: isSuccessResult(lastResult) ? lastResult.value.api : 'guardians',
-      text: isSuccessResult(lastResult) ? lastResult.value.text : '',
-      score_threshold: isSuccessResult(lastResult)
-        ? lastResult.value.score_threshold
-        : 0.5,
-      model: isSuccessResult(lastResult)
-        ? lastResult.value.model
-        : 'gpt-4o-mini',
-      result: isSuccessResult(lastResult) ? lastResult.value.result : '',
+      type: isSuccessResult(lastResult) ? lastResult.value.type : 'text',
     },
   })
 
   return (
-    <div className="flex flex-wrap gap-3 rounded border p-4">
+    <div className="rounded border p-4">
       <form
         method="POST"
         {...getFormProps(form)}
-        action={action}
         className="mx-auto max-w-2xl flex-grow space-y-4"
       >
         <Field>
@@ -83,76 +69,31 @@ export function PlaygroundForm() {
         </Field>
         {fields.api.value === 'guardians' && (
           <Field>
-            <Label htmlFor={fields.score_threshold.id}>しきい値</Label>
-            <div className="flex flex-wrap items-center gap-3 md:flex-nowrap">
-              <SliderConform
-                meta={fields.score_threshold}
-                max={1}
-                step={0.1}
-                min={0.1}
-              />
-              <ScoreThresholdDescription
-                value={Number(fields.score_threshold.value)}
-              />
-            </div>
-            {fields.score_threshold.errors && (
-              <FieldError>{fields.score_threshold.errors}</FieldError>
+            <Label htmlFor={fields.type.id}>タイプ</Label>
+            <SelectConform
+              placeholder="タイプを選択"
+              meta={fields.type}
+              items={[
+                { value: 'text', name: 'テキスト' },
+                {
+                  value: 'image',
+                  name: '画像',
+                },
+              ]}
+            />
+            {fields.type.errors && (
+              <FieldError>{fields.type.errors}</FieldError>
             )}
           </Field>
         )}
-        <Field>
-          <Label htmlFor={fields.model.id}>使用するAIモデル</Label>
-          <SelectConform
-            placeholder="AIモデルを選択"
-            meta={fields.model}
-            items={[
-              { value: 'gpt-4o-mini', name: 'GPT-4o mini', Icon: SiOpenai },
-              {
-                value: 'claude-3-haiku',
-                name: 'Claude 3 Haiku',
-                Icon: SiAnthropic,
-              },
-              {
-                value: 'gemini-1.5-flash',
-                name: 'Gemini 1.5 Flash',
-                Icon: SiGoogle,
-              },
-            ]}
-          />
-          {fields.model.errors && (
-            <FieldError>{fields.model.errors}</FieldError>
-          )}
-        </Field>
-        <Field>
-          <Label htmlFor={fields.text.id}>テキスト</Label>
-          <TextareaConform
-            meta={fields.text}
-            className="h-auto min-w-full border text-base"
-            autoFocus
-          />
-          {fields.text.errors && <FieldError>{fields.text.errors}</FieldError>}
-        </Field>
-
-        <div className="flex gap-2">
-          <Button
-            disabled={isPending}
-            className="focus:outline-primary w-full bg-teal-500 hover:bg-teal-600"
-            size="lg"
-          >
-            <PlayIcon className="mr-2 h-5 w-5" />
-            APIを試す
-            {isPending && (
-              <Loader2Icon className="ml-2 h-5 w-5 animate-spin ease-in-out" />
-            )}
-          </Button>
-        </div>
       </form>
-      {isSuccessResult(lastResult) && !lastResult.value.isLimitReached && (
-        <ResultSyntax code={lastResult.value.result} />
+      {fields.api.value === 'guardians' && fields.type.value === 'text' && (
+        <GuardianTextForm />
       )}
-      {isSuccessResult(lastResult) && lastResult.value.isLimitReached && (
-        <LimitReachedDialog />
+      {fields.api.value === 'guardians' && fields.type.value === 'image' && (
+        <GuardianImageForm />
       )}
+      {fields.api.value === 'sunshines' && <SunshineForm />}
     </div>
   )
 }
